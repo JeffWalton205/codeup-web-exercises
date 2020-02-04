ed79e46b3014b94f79d059a2a0da279c90ed7027
Jeff Walton <jeff.walton205@gmail.com>
2020-02-04 15:59:51 -0600

    HTML JS exercise

---
+++ js/external.js
@@ -0,0 +1,26 @@
+"use strict";
+
+// alert("Welcome to my Website!");
+//
+// var userInput = prompt("What is your favorite color?");
+//
+// alert("What a coincidence, " + userInput + " is also my favorite color!");
+
+// var userInputOne = Number(prompt("Days you rented the Little Mermaid:"));
+// var userInputTwo = Number(prompt("Days you rented Brother Bear:"));
+// var userInputThree = Number(prompt("Days you rented Hercules:"));
+// var sumTotal = (userInputOne * 3) + (userInputTwo * 3) + (userInputThree * 3);
+// alert("Your total is: $" + sumTotal);
+
+// var googlePay = 400;
+// var AmazonPay = 380;
+// var fbPay = 350;
+// var userInputOne = Number(prompt("How many hours for Google?"));
+// var userInputTwo = Number(prompt("How many hours for Amazon?"));
+// var userInputThree = Number(prompt("How many hours for Facebook?"));
+// var sumTotal = (userInputOne * googlePay) + (userInputTwo * AmazonPay) + (userInputThree * fbPay);
+// alert("You're total paycheck is $" + sumTotal);
+
+
+var classTime = confirm("Is your class at 8AM?");
+var classFull = false;
