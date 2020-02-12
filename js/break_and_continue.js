194405b445e2a0e51043498f985f590c21a7db3e
Jeff Walton <jeff.walton205@gmail.com>
2020-02-12 12:15:00 -0600

    Loops exercises

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -14,16 +14,39 @@
 //     }
 // }
 
-var numberToStopAt = prompt("Enter an odd number 1-50");
+var numberToStopAt = Number(prompt("Enter an odd number 1-50"));
 
 for (var i = 1; i < 50; i+=2) {
 
     console.log('Loop counter is: ' + i);
 
     if (i === numberToStopAt) {
-        console.log('We have reached the stopping point: break!');
-        // use the break keyword to exit from the while loop
-        break;
+        console.log('Yikes! Skipping number ' + numberToStopAt);
+        continue;
 
     }
 }
+
+// do {
+//     var userNumber = Number(prompt("1-50 Odd"))
+//     if (userNumber < 1 || userNumber > 50) {
+//         alert("Invalid");
+//     } else if (userNumber % 2 === 0){
+//         alert(userNumber + " is invalid");
+//     } else if (isNaN(userNumber)){
+//         alert(userNumber + "Is not a number");
+//     } else {
+//         alert("Okay, let's do this.");
+//     }
+// } while (true);
+//
+// for(var i = 1; i<= 50; i++) {
+//     if (i % 2 === 0){
+//         continue;
+//     }
+//     if (i===userNumber){
+//         console.log("Skipping number" + userNumber);
+//     } else {
+//         console.log("Here is an odd number" + i);
+//     }
+// }
--- js/while.js
+++ js/while.js
@@ -24,6 +24,13 @@
 //     console.log(Math.pow(2, i));
 //     i++;
 // }
+// **********Instructor SolutioN!!!!!!!!!
+// var i = 2;
+// while (i <= 65536) {
+//     console.log(i);
+//     i *=2;
+// }
+
 
 // An ice cream seller can't go home until she sells all of her cones. Write a JS program that generates a random number between 50 and 100 representing the amount of cones to sell. Your code should generate numbers between 1 and 5, simulating the amount of cones being bought by her clients. Use a do-while loop to log to the console the amount of cones sold to each person. This is how you get the random numbers.
 //
@@ -38,9 +45,26 @@
 // Cannot sell you 6 cones I only have 3...  // If there are not enough cones
 //     Yay! I sold them all! // If there are no more cones
 
-var allCones = Math.floor(Math.random() * 50) + 50;
-var toSell = Math.floor(Math.random() * 5) + 1;
-
-do {
-
-}
+// var totalCones = Math.floor(Math.random() * 50) + 50;
+// var i = Math.floor(Math.random() * 5) + 1;
+//
+// do {
+//     console.log(totalCones);
+//     console.log(i);
+//     console.log(i + ' cones sold...');
+//     i++;
+// } while (i <= totalCones);
+// ****************Instructor Solution!**************
+// var allCones = Math.floor(Math.random() * 50) + 50;
+// do{
+//     var conesPurchased = Math.floor(Math.random() * 5) + 1;
+//     if (conesPurchased > allCones){
+//         var sadMessage = "Cannot sell you " + conesPurchased + " I only have " +            allCones + " left";
+//         console.log(sadMessage);
+//     } else {
+//         allCones -= conesPurchased;
+//         var happyMessage = conesPurchased + " cones sold..." + allCones + " cones to            go!";
+//         console.log(happyMessage);
+//     }
+// } while (allCones > 0);
+//     console.log("Yay I sold all my cones!");
