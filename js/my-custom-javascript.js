e2b99c31783d08d4b08187a7b30a7cb02c4c5631
Jeff Walton <jeff.walton205@gmail.com>
2020-02-27 08:30:52 -0600

    Exercises

--- js/my-custom-javascript.js
+++ js/my-custom-javascript.js
@@ -1,5 +1,14 @@
 "use strict";
 
-$(document).ready(function() {
-    alert("The DOM has finished loading!");
-    });
+// $(document).ready(function() {
+//     alert("The DOM has finished loading!");
+//     });
+//
+// var header = $('#header').html();
+// alert(header);
+//
+// var body = $('.list').html();
+// alert(body);
+
+
+
